3938c1d18bcde35feae856de103c1880cb8c0c83
Mindaugas Straksys <100799754+eyyMinda@users.noreply.github.com>
2022-08-03 00:57:44 +0300

    Insert, Select in todoapp DB

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 import express from "express";
 import handlebars from "express-handlebars";
 import mysql from 'mysql2';
+import mainPage from './lib/mainPage.js';
 
 const app = express();
 const port = 8080;
-
 app.use(express.static('public'));
+
 app.set('view engine', 'hbs');
 app.engine('hbs', handlebars.engine({
-    layoutsDir: 'views/layouts',
     extname: 'hbs'
 }));
+
+app.get('/', mainPage);
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -20,11 +22,12 @@ const connection = mysql.createConnection({
     password: '123456789'
 })
 
-app.get('/', (req, res) => res.render('index'));
 app.get('/db', (req, res) => {
     connection.execute('SELECT productLine, textDescription FROM productlines', (err, rows) => {
         const data = rows.map(row => row);
         res.render('db', { data: data });
     });
 })
+
+
 app.listen(port, () => console.log(`Starting server on http://localhost:${port}`));
